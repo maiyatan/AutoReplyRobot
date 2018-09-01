a6467a33759e6cf4754da5f97b373f97bfbbb0ae
youtan <tan70219@gmail.com>
2018-09-01 17:09:59 +0900

    可以查询了

--- src/com/imooc/dao/MessageDao.java
+++ src/com/imooc/dao/MessageDao.java
@@ -14,18 +14,23 @@ import com.imooc.db.ConnDb;
  */
 public class MessageDao {
 	/**
-	 * 获取message表的全部数据
+	 * 根据查询条件查询消息列表
 	 */
-	public List<Message> queryMessageList() {
+	public List<Message> queryMessageList(String command, String description) {
 		SqlSession session = null;
 		ConnDb connDb = new ConnDb();
 		List<Message> messageList = new ArrayList<Message>();
 		try {
+			// 将command和description放入message，作为参数传递给sql去处理
+			Message message = new Message();
+			message.setCommand(command);
+			message.setDescription(description);
+
 			session = connDb.getSqlSession();
 			// 映射sql的标识字符串
 			String statement = "com.imooc.sql.Message.queryMessageList";
 			// 执行查询返回一个唯一user对象的sql
-			messageList = session.selectList(statement);
+			messageList = session.selectList(statement, message);
 		} catch (Exception e) {
 			// TODO: handle exception
 		} finally {
@@ -34,13 +39,5 @@ public class MessageDao {
 			}
 		}
 		return messageList;
-	}
-	
-	public static void main(String[] args) {
-		MessageDao messageDao=new MessageDao();
-		List<Message> messageList = messageDao.queryMessageList();
-		for (Message message : messageList) {
-			System.out.println("getContent: "+message.getContent());
-		}
 	}
 }
--- src/com/imooc/servlet/ListServlet.java
+++ src/com/imooc/servlet/ListServlet.java
@@ -11,13 +11,26 @@ import javax.servlet.http.HttpServletResponse;
 import com.imooc.bean.Message;
 import com.imooc.service.MessageService;
 
+/**
+ * 页面初始化处理
+ * 
+ * @author youtan
+ *
+ */
 public class ListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		MessageService messageService=new MessageService();
-		List<Message> messageList = messageService.queryMessageList();
+		// 接收页面传过来的参数
+		String command = req.getParameter("command");
+		String description = req.getParameter("description");
+		// 向页面传值。目的是：搜索栏保存查询条件
+		req.setAttribute("command", command);
+		req.setAttribute("description", description);
+
+		MessageService messageService = new MessageService();
+		List<Message> messageList = messageService.queryMessageList(command, description);
 		req.setAttribute("messageList", messageList);
 		// 页面跳转
 		req.getRequestDispatcher("/WEB-INF/jsp/back/list.jsp").forward(req, resp);
